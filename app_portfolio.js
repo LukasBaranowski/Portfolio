60cfe1a9c2f3e46e4281287592aead147018a0ab
Lukas <54035973+LukasBaranowski@users.noreply.github.com>
2020-07-12 00:54:29 +0200

    Update app_portfolio.js

--- app_portfolio.js
+++ app_portfolio.js
@@ -4,6 +4,7 @@ let menu = document.querySelector('.toggler');
 function playVideo () {
     menu.addEventListener('click', () => {
         menuVideo.play();
+        menuVideo.playbackRate = 2;
         menuVideo.style.zIndex = '5';
         menu.style.visibility = 'hidden';
     });
