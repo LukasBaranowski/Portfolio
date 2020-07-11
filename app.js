7ed3fc96106a9ddc59c292bdd8683f7d5ffcf157
Lukas <54035973+LukasBaranowski@users.noreply.github.com>
2020-07-11 11:35:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+let unicorn = document.querySelector('.unicorn');
+let textBox = document.querySelector('.textBox');
+
+let star = document.createElement('i');
+let x = Math.floor(Math.random()*window.innerWidth);
+let duration = Math.random() * 2.5;
+let h = Math.random() * 100;
+let amount = 50;
+let bg = document.querySelector('.bg');
+let i = 0;
+
 function stars () {
-    let amount = 50;
-    let bg = document.querySelector('.bg');
-    let i = 0;
+    
     while (i < amount){
-        let star = document.createElement('i');
-        let x = Math.floor(Math.random()*window.innerWidth);
-
-        let duration = Math.random() * 2.5;
-        let h = Math.random() * 100;
-
         star.style.left = x + 'px';
         star.style.width = 0.5 + 'px';
         star.style.height =  5 + h + 'px';
@@ -18,4 +21,16 @@ function stars () {
         i++
     }
 }
-stars()
+
+function fly () {
+     unicorn.addEventListener('click', function () {
+        unicorn.classList.add('fly');
+    })
+    textBox.addEventListener('click', function () {
+        unicorn.classList.remove('fly');
+    })
+}
+
+
+fly();
+stars();
