0b24f3a1a3f2954a9f7d5f8b283c024dac254ae1
Lukas <54035973+LukasBaranowski@users.noreply.github.com>
2020-07-12 09:10:23 +0200

    Update app_portfolio.js

--- app_portfolio.js
+++ app_portfolio.js
@@ -1,12 +1,14 @@
 let menuVideo = document.querySelector('.menu_video');
 let menu = document.querySelector('.toggler');
+let burger = document.querySelector('.burger');
 
 function playVideo () {
     menu.addEventListener('click', () => {
         menuVideo.play();
         menuVideo.playbackRate = 3;
         menuVideo.style.zIndex = '5';
-        menu.style.visibility = 'hidden';
+        burger.style.visibility = 'hidden';
+        
     });
 }
 playVideo();
